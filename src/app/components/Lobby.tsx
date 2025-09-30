6dc8d8d4c001432a7fa9bd284bbed9a78d429fa0
Sebastian Barrientos <sebastianbarrientos@coolx.earth>
2025-09-30 16:29:59 +0200

    Eliminando comentarios

--- src/app/components/Lobby.tsx
+++ src/app/components/Lobby.tsx
@@ -74,24 +74,6 @@ const Lobby = () => {
             </div>
         </div>
 
-        //   <div className="flex flex-col items-center justify-center text-slate-400  font-medium px-6 py-8 rounded-md shadow-lg max-w-lg mx-auto">
-        //     <h1 className="text-2xl font-bold text-slate-200 mb-4">
-        //       Room ID: <span className="text-sky-500">{roomId}</span>
-        //     </h1>
-        //     <p className="text-lg text-slate-400 mb-2 text-center">
-        //       Share the Room ID with the other participants.
-        //     </p>
-        //     <p className="text-lg text-slate-400 mb-6 text-center">
-        //       Connected participants: <span className="text-sky-500 font-semibold">{room.users?.length}</span>
-        //     </p>
-        //     <button
-        //       onClick={handleReady}
-        //       className="bg-slate-950 text-slate-400 border border-slate-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
-        //     >
-        //       <span className="bg-slate-400 shadow-slate-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
-        //       Start Matching
-        //     </button>
-        //   </div>
     );
 };
 
